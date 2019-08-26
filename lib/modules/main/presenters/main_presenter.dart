475f35adf4aa5b5ccc41891bb0232d553dc3303a
Invisgoth <hectorjdg1990@gmail.com>
2019-08-26 16:11:44 -0300

    Removed unused class

--- lib/modules/main/presenters/main_presenter.dart
+++ lib/modules/main/presenters/main_presenter.dart
@@ -4,13 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:notish/shared_state/shared_memory.dart';
 import 'package:rxdart/rxdart.dart';
 
-abstract class MainViewDelegate {
-  void onUserLogged();
-  void noLogged();
-}
-
 class MainPresenter {
   Observable streamIsLogged = SharedMemory.streamIsLogged.stream;
-
   MainPresenter();
 }
